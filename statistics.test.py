2c562b422eab03dee7cc87f1ad4d10908f7f7aa4
nisharai1 <105203884+nisharai1@users.noreply.github.com>
2022-05-16 14:18:23 +0530

    Updated the fix

--- statistics.test.py
+++ statistics.test.py
@@ -3,14 +3,14 @@ from statistics import *
 
 class StatsTest(unittest.TestCase):
   def test_report_min_max_avg(self):
-    computedStats = statistics.calculateStats([1.5, 8.9, 3.2, 4.5])
+    computedStats = calculateStats([1.5, 8.9, 3.2, 4.5])
     epsilon = 0.001
     self.assertAlmostEqual(computedStats["avg"], 4.525, delta=epsilon)
     self.assertAlmostEqual(computedStats["max"], 8.9, delta=epsilon)
     self.assertAlmostEqual(computedStats["min"], 1.5, delta=epsilon)
 
   def test_avg_is_nan_for_empty_input(self):
-    computedStats = statistics.calculateStats([])
+    computedStats = calculateStats([])
     # All fields of computedStats (average, max, min) must be
     # nan (not-a-number), as defined in the math package
     # Design the assert here.
